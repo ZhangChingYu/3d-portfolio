bc0db45d01e0daef68a322ab815004b2b05a8152
Silvia Chang <sunny1218.chang@gmail.com>
2025-03-02 15:14:37 +1100

    Develope Star Background

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -22,7 +22,7 @@ const Contact = () => {
   return (
     <div className='xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden'>
       <motion.div variants={slideIn('left', 'tween', 0.2, 1)}
-      className='flex-[0.75] bg-black-100 p-8 rounded-2xl'>
+      className='flex-[0.8] bg-black-100 p-8 rounded-2xl'>
         <p className={`${styles.sectionSubText}`}>Gets in touch</p>
         <h3 className={`${styles.sectionHeadText}`}>Contact.</h3>
         <form ref={formRef} onSubmit={handleSubmit} className='mt-12 flex flex-col gap-8'>
