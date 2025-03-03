b3ed1cc65e09e83ce505377191fa3d8ea6586988
Silvia Chang <sunny1218.chang@gmail.com>
2025-03-03 13:24:49 +1100

    Adjust Hero Styling

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,7 +20,7 @@ const Hero = () => {
         </div>
       </div>
       <ComputersCanvas />
-      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
+      <div className='absolute xs:bottom-10 bottom-20 w-full flex justify-center items-center'>
         <a href='#about'>
           <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center item-start p-2'>
             <motion.div className='w-3 h-3 rounded-full bg-secondary mb-1'
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -14,7 +14,7 @@ const Computers = ({ isMobile }) => {
       <pointLight intensity={1}/>
       <pointLight intensity={1}/>
       <spotLight intensity={1} position={[-20, 50, 10]} angle={0.12} penumbra={1} castShadow/>
-      <primitive object={computer.scene} scale={isMobile ? 0.5 : 0.65} position={isMobile ? [0, -3, -2.2]:[0, -3.25, -1.5]} rotation={[-0.01, -0.2, -0.1]}/>
+      <primitive object={computer.scene} scale={isMobile ? 0.5 : 0.65} position={isMobile ? [0, -2.5, -1]:[0, -3.25, -1.5]} rotation={[-0.01, -0.2, -0.1]}/>
     </mesh>
   )
 }
